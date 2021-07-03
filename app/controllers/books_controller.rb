a5936c50dc8ffb23b91b31d148349c292936847b
EC2 Default User <ec2-user@ip-172-31-94-81.ec2.internal>
2021-07-03 11:02:51 +0000

    [Start]3-8_basic function complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,21 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully created"
-      redirect_to books_path(@book)
+      redirect_to book_path(@book.id)
     else
-        @books = Book.all
-        render 'index'
-    end    
+      @books = Book.all
+      render 'index'
+    end  
   end
   
   def edit
@@ -27,19 +23,17 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      flash[:notice] = "Book was successfully updated"
-      redirect_to books_path
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
     else
-      render 'edit'
-    end
+      render action: :edit
+    end  
   end
   
   def destroy
     book = Book.find(params[:id]) 
     book.destroy  
-    flash[:notice] = "Book was successfully destroyed"
     redirect_to books_path  
   end  
   
